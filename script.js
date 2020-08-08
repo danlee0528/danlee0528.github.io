8e6c9b348de014acfb68a2d014d9cb5cb289c3c6
Daniel Lee <hla191@sfu.ca>
2020-08-07 18:17:32 -0700

    revise navbar and image responsiveness

--- script.js
+++ script.js
@@ -11,8 +11,8 @@ $(document).ready(function(){
     $(window).scroll(function(){
       var scroll = $(window).scrollTop();
     //   On Curriculum Section
-        if (scroll > $(window).innerHeight() * 2 && scroll < $(window).innerHeight() * 4){
-          $("#navbar").css({"background":"rgba(0, 0, 0, 0.6)", "transition":"0.3s ease-in-out"});
+        if (scroll > $(window).innerHeight() * 2 && scroll < $(window).innerHeight() * 5){
+          $("#navbar").css({"display":"block","background":"rgba(0, 0, 0, 0.6)", "transition":"0.3s ease-in-out"});
           $('.navbar-ul').css({"color":"white","padding":"2em","transition":"0.3s ease-in-out"});
           $('.navbar-li').css({"color":"white","padding-right":"5em","transition":"0.3s ease-in-out"});
           $('.navbar-li').hover(function(){
@@ -22,8 +22,8 @@ $(document).ready(function(){
           });
         }
         // First on About section and second on Portfolio Section
-        else if((scroll >= $(window).innerHeight() * 1 && scroll < $(window).innerHeight() * 2)|| (scroll >= $(window).innerHeight() * 4 && scroll < $(window).innerHeight() * 8)){
-            $("#navbar").css({"background":"rgba(255, 255, 255, 0.6)","transition":"0.3s ease-in-out","box-shadow":"0 2px 2px -2px rgba(0,0,0,.2)"}); 
+        else if((scroll >= $(window).innerHeight() * 1 && scroll < $(window).innerHeight() * 2)|| (scroll >= $(window).innerHeight() * 5 && scroll < $(window).innerHeight() * 6)){
+            $("#navbar").css({"display":"block","background":"rgba(255, 255, 255, 0.6)","transition":"0.3s ease-in-out","box-shadow":"0 2px 2px -2px rgba(0,0,0,.2)"}); 
             $('.navbar-ul').css({"color":"#6F6F6F","padding":"2em","transition":"0.3s ease-in-out"});
             $('.navbar-li').css({"color":"#6F6F6F","padding-right":"5em","transition":"0.3s ease-in-out"});
             $('.navbar-li').hover(function(){
@@ -32,8 +32,11 @@ $(document).ready(function(){
                 $(this).css({"color":"#6F6F6F", "transition":"0.3s ease-in"});
               });
         }
+        else if((scroll >= $(window).innerHeight() * 6 && scroll < $(window).innerHeight() * 8)){
+          $("#navbar").css({"display":"none"});
+      }
         else{
-            $("#navbar").css({"background":"transparent","transition":"0.3s ease-in-out", "box-shadow":"none"});   
+            $("#navbar").css({"display":"block","background":"transparent","transition":"0.3s ease-in-out", "box-shadow":"none"});   
             $('.navbar-ul').css({"color":"#6F6F6F","padding":"4em","transition":"0.3s ease-in-out"});
             $('.navbar-li').css({"color":"#6F6F6F","padding-right":"5em","transition":"0.3s ease-in-out"});
             $('.navbar-li').hover(function(){
