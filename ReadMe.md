16a796eb02e2343770336a355b80157db03661f7
danlee0528 <danlee0528@hotmail.com>
2020-11-21 19:49:05 -0800

    reworked v1

--- ReadMe.md
+++ ReadMe.md
@@ -1,1 +1,8 @@
-Website Link: https://danlee0528.github.io/
+# Information
+This is my personal website built by using Bootsrap4 framework. I used desktop-first or max-width prioritized media queries to support reponsivenss of my website. Please feel free to provide feedbacks. In the meantime, I hope you enjoy your time exploring it!
+
+# Media Sources
+Icons and images used for the website don't belong to myself, but they are from the following websites:
+- Icons: https://www.flaticon.com/
+- Images: https://unsplash.com/
+
--- script.js
+++ script.js
@@ -1,49 +1,7 @@
+function openNav() {
+  document.getElementById("myNav").style.width = "100%";
+}
 
-$(document).ready(function(){
-  $(".landing-grey-area").animate({height: 'show'}, 1000);
-  $(".landing-content-container").delay(1000).animate({opacity: "1"}, 1500);
-
-  $(".parallax").ripples({
-    resolution: 300,
-    perturbance: .01,
-  });
-
-    $(window).scroll(function(){
-      var scroll = $(window).scrollTop();
-    //   On Curriculum Section
-        if (scroll > $(window).innerHeight() * 2 && scroll < $(window).innerHeight() * 5){
-          $("#navbar").css({"display":"block","background":"rgba(0, 0, 0, 0.6)", "transition":"0.8s ease-in-out"});
-          $('.navbar-ul').css({"color":"white","padding":"2em","transition":"0.6s ease-in-out"});
-          $('.navbar-li').css({"color":"white","padding-right":"5em","transition":"1s ease-in-out"});
-          $('.navbar-li').hover(function(){
-            $(this).css({"color":"#6443FA", "transition":"0.3s ease-in", "font-weight":"800"});
-          }, function(){
-            $(this).css({"color":"white", "transition":"0.3s ease-in"});
-          });
-        }
-        // First on About section and second on Portfolio Section
-        else if((scroll >= $(window).innerHeight() * 1 && scroll < $(window).innerHeight() * 2)|| (scroll >= $(window).innerHeight() * 5 && scroll < $(window).innerHeight() * 6)){
-            $("#navbar").css({"display":"block","background":"rgba(255, 255, 255, 0.6)","transition":"0.6s ease-in-out","box-shadow":"0 2px 2px -2px rgba(0,0,0,.2)"}); 
-            $('.navbar-ul').css({"color":"#6F6F6F","padding":"2em","transition":"1s ease-in-out"});
-            $('.navbar-li').css({"color":"#6F6F6F","padding-right":"5em","transition":"1s ease-in-out"});
-            $('.navbar-li').hover(function(){
-                $(this).css({"color":"#6443FA", "transition":"0.3s ease-in", "font-weight":"800"});
-              }, function(){
-                $(this).css({"color":"#6F6F6F", "transition":"0.3s ease-in"});
-              });
-        }
-        else if(scroll >= $(window).innerHeight() * 6){
-          $("#navbar").css({"display":"none"});
-      }
-        else{
-            $("#navbar").css({"display":"block","background":"transparent","transition":"0.6s ease-in-out", "box-shadow":"none"});   
-            $('.navbar-ul').css({"color":"#6F6F6F","padding":"4em","transition":"1s ease-in-out"});
-            $('.navbar-li').css({"color":"#6F6F6F","padding-right":"5em","transition":"1s ease-in-out"});
-            $('.navbar-li').hover(function(){
-                $(this).css({"color":"#6443FA", "transition":"0.3s ease-in", "font-weight":"800"});
-              }, function(){
-                $(this).css({"color":"#6F6F6F", "transition":"0.3s ease-in"});
-              });
-        }
-    })
-  })
+function closeNav() {
+  document.getElementById("myNav").style.width = "0%";
+}
