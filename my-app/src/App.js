fd9550a27b84a7ae648edb9f21412f9c3dc83422
Daniel Lee <dlee0528@hotmail.com>
2021-07-06 00:35:16 -0700

    install react and other plugins

---
+++ my-app/src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <title>Daniel's Website</title>
+      </header>
+
+    Hello World
+    </div>
+  );
+}
+
+export default App;
